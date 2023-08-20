9cf1e816ddfffcc677ffcacbf46d4d6b0ffcdd1e
kumudiaorong <kmdr.error@gmail.com>
2023-08-20 18:13:57 +0800

    v0.0.3 single

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 if [ $1 = "clean" ]; then
-    rm -rf build
     rm -rf cpp
     exit 0
 fi
 cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S$PWD -Bbuild -G Ninja
 cmake --build build --config Release --target all --
+rm -rf build
